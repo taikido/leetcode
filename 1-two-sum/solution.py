5a6da113a4d5ddc848d496375c12f65857d95b61
taikido <taikido@users.noreply.github.com>
2015-01-31 23:22:04 -0500

    Initial commit for leetcode problem 1

---
+++ 1-two-sum/solution.py
@@ -0,0 +1,18 @@
+"""============================================================================
+* Name : solution.py
+* Author: taikido
+* Description: Leetcode Problem 1: Two Sum
+* Source: https://oj.leetcode.com/problems/two-sum/
+* Date: Jan.2015
+*==========================================================================="""
+
+class Solution:
+    # @return a tuple, (index1, index2)
+    def twoSum(self, num, target):
+        d = {}
+        for i in range (len(num)):
+            j=target - num [i]
+            if j in d.keys():
+                idx = d[j]
+                return (i+1, idx+1) if i < idx else (idx+1,i+1)
+            d [num [i]] = i
